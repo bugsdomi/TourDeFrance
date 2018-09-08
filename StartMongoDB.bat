3f904da39ffabc44e8040ffd53041882dd5ed4ce
BugsDomi <bugsdomi@neuf.fr>
2018-09-08 05:37:11 +0200

    Ajout du lanceur de MongoDB

---
+++ StartMongoDB.bat
@@ -0,0 +1,5 @@
+d:
+cd "D:/Cours Web/Cours/IFOCOP/Cours/DH/mongodb/server/bin"
+start cmd /k mongod.exe --dbpath="D:/Cours Web/Cours/IFOCOP/Cours/DH/Ateliers/02 - Atelier Back/TourDeFrance/data"
+pause 10
+start cmd /k mongo.exe
