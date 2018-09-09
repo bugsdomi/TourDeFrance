c411c2cc3eb663281de1ff1bffef8bff603ad829
BugsDomi <bugsdomi@neuf.fr>
2018-09-09 09:53:48 +0200

    Transfert sur mLab de la BDD

--- StartMongoDB.bat
+++
@@ -1,5 +0,0 @@
-d:
-cd "D:/Cours Web/Cours/IFOCOP/Cours/DH/mongodb/server/bin"
-start cmd /k mongod.exe --dbpath="D:/Cours Web/Cours/IFOCOP/Cours/DH/Ateliers/02 - Atelier Back/TourDeFrance/data"
-pause 10
-start cmd /k mongo.exe
